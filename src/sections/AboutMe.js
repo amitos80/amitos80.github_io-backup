163601389ecb787a5d343689e61f5a90d07ea567
amitos80 <amitos80@gmail.com>
2020-04-28 03:41:38 +0300

    seo

--- src/sections/AboutMe.js
+++ src/sections/AboutMe.js
@@ -3,20 +3,20 @@ import React from "react";
 
 
 const AboutMe = () => {
-    const intro = `Hey there!
+    const __html = `<div>Hey there!</div>
+            <div>I'm Amit Friedberg. I'm a Software development enthusiast with a passion for web development and the js echo-system.</div>
 
-            I'm Amit Friedberg. I'm a Software development enthusiastic with passion for web development and the js echo-system.
-            I've been a developer for the past 9 Years. I've been a full stack web developer in the music, adtech, fintech industries and as a freelancer ([download cv](https://drive.google.com/open?id=1nm-MPpHGrxKl3TsAj8YpV8c8IQwETykV)).
-            As a developer I always try to come up with the most simple and elegant solution according to the constraints and requirements.
-            I'm Skilled in JS, React, Vue, Styled Components, Nodejs, Webpack, mondoDb, Software Architecture, Atomic Design Methodology.`
+            <div>I've been a developer for the past 9 Years. During that period I gained lots of experience in developing fast,
+             beautiful, responsive web apps in multiple roles and companies in the music, adtech, journalism, and fintech industries.
+             Nowadays work as a freelancer web developer giving development and consulting services for companies. <a style="font-size: 14px" href="https://drive.google.com/open?id=1nm-MPpHGrxKl3TsAj8YpV8c8IQwETykV" target="_blank">(check out my cv)</a></div>
+
+            <div>As a developer, I always try to come up with the most simple and elegant solution according to the constraints and requirements.
+             I'm Skilled in JS, React, Vue, Styled Components, Nodejs, Webpack, MongoDB, Software Architecture, Atomic Design Methodology and always love to learn new technologies.</div>`
+
     return (
         <section id="about-me">
-
-
             <div className="grid lg:grid-cols-6 gap-12 items-center">
-                <h2 className="text-justify md:col-span-4 lg:col-span-6 wow fadeIn">
-                    {intro}
-                </h2>
+                <h2 dangerouslySetInnerHTML={{ __html }} className="text-justify md:col-span-4 lg:col-span-6 wow fadeIn" />
             </div>
         </section>
     );
