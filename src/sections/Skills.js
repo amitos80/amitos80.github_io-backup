fff37202944e0e557cd9228dfe881998afe91437
amitos80 <amitos80@gmail.com>
2020-05-02 06:00:41 +0300

    alt minify

--- src/sections/Skills.js
+++ src/sections/Skills.js
@@ -14,6 +14,7 @@ const Skills = () => {
             id
             name
             tech
+            alt
             icon {
               childImageSharp {
                 fixed(width: 20, height: 20) {
@@ -41,6 +42,7 @@ const Skills = () => {
           >
             <div className="flex items-center">
               <GatsbyImage
+                alt={node.alt}
                 loading="lazy"
                 className="w-5 h-5 mr-5"
                 {...node.icon.childImageSharp}
